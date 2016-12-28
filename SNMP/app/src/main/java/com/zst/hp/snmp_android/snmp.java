399e15226668bf74b380f42f788b05f427bf58c3
michakrzeminski <wiorek455@wp.pl>
2016-12-28 16:22:34 +0100

    Android hello world

---
+++ SNMP/app/src/main/java/com/zst/hp/snmp_android/snmp.java
@@ -0,0 +1,13 @@
+package com.zst.hp.snmp_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class snmp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snmp);
+    }
+}
