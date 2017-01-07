5a96793b85e24c7e1506cf7e5fd162ffb05b856b
michakrzeminski <wiorek455@wp.pl>
2017-01-07 22:49:30 +0100

    Android main activity + settings + swap

--- SNMP/app/src/main/java/com/zst/hp/snmp_android/snmp.java
+++ SNMP/app/src/main/java/com/zst/hp/snmp_android/snmp.java
@@ -1,13 +1,150 @@
 package com.zst.hp.snmp_android;
 
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Vector;
 
-public class snmp extends AppCompatActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.Toast;
+
+public class snmp extends Activity {
+
+    ListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listFolders;
+    HashMap<String, List<String>> listChild;
+
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snmp);
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.listview);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ListAdapter(this, listFolders, listChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        gestureDetector = new GestureDetector(new SwipeGestureDetector());
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gestureDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void onLeftSwipe() {
+        Intent intent = new Intent(snmp.this, Settings.class);
+        startActivity(intent);
+    }
+
+    private void onRightSwipe() {
+        //TODO
+    }
+
+    private void prepareListData() {
+        listFolders = new ArrayList<String>();
+        listChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listFolders.add("System");
+        listFolders.add("Interfaces");
+        listFolders.add("Ip");
+        listFolders.add("Icmp");
+        listFolders.add("Tcp");
+        listFolders.add("Udp");
+        listFolders.add("Snmp");
+        listFolders.add("Host");
+
+        List<String> system = new ArrayList<String>();
+        system.add("Test1");
+
+        List<String> interfaces = new ArrayList<String>();
+        interfaces.add("Test2");
+
+        List<String> ip = new ArrayList<String>();
+        ip.add("Test3");
+
+        List<String> icmp = new ArrayList<String>();
+        ip.add("Test4");
+
+        List<String> tcp = new ArrayList<String>();
+        ip.add("Test5");
+
+        List<String> udp = new ArrayList<String>();
+        ip.add("Test6");
+
+        List<String> snmp = new ArrayList<String>();
+        ip.add("Test7");
+
+        List<String> host = new ArrayList<String>();
+        ip.add("Test8");
+
+        listChild.put(listFolders.get(0), system);
+        listChild.put(listFolders.get(1), interfaces);
+        listChild.put(listFolders.get(2), ip);
+        listChild.put(listFolders.get(3), icmp);
+        listChild.put(listFolders.get(4), tcp);
+        listChild.put(listFolders.get(5), udp);
+        listChild.put(listFolders.get(6), snmp);
+        listChild.put(listFolders.get(7), host);
+    }
+
+    private class SwipeGestureDetector extends GestureDetector.SimpleOnGestureListener {
+        // Swipe properties, you can change it to make the swipe
+        // longer or shorter and speed
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 200;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                float diffAbs = Math.abs(e1.getY() - e2.getY());
+                float diff = e1.getX() - e2.getX();
+
+                if (diffAbs > SWIPE_MAX_OFF_PATH)
+                    return false;
+
+                // Left swipe
+                if (diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    snmp.this.onLeftSwipe();
+
+                    // Right swipe
+                } else if (-diff > SWIPE_MIN_DISTANCE
+                        && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    snmp.this.onRightSwipe();
+                }
+            } catch (Exception e) {
+                Log.e("Snmp", "Error on gestures");
+            }
+            return false;
+        }
+    }
+
 }
